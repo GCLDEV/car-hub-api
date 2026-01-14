e19edc612b2effc0d452f7055962018b0fc05f89
gcldev <gabrielcarvalho850@outlook.com>
2026-01-14 15:24:41 -0300

    Adiciona configurações do Vite e ajusta variáveis de ambiente no docker-compose.yml para estabilidade do esbuild

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    host: true,
+    port: 1337,
+    hmr: {
+      port: 1337
+    }
+  },
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.ts': 'tsx'
+      }
+    }
+  },
+  esbuild: {
+    logOverride: {
+      'this-is-undefined-in-esm': 'silent'
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['fsevents']
+    },
+    chunkSizeWarningLimit: 2000
+  }
+})
